d9a83884037dbb2ef0a7ac4774ed6f63faafb544
yellia <865756739@qq.com>
2022-10-08 09:01:13 +0800

    1

---
+++ src/main/scala/com/yk/sparkcore/com/yk/sparkcore/basic/Accumulator.scala
@@ -0,0 +1,20 @@
+package com.yk.sparkcore.com.yk.sparkcore.basic
+
+import org.apache.spark.sql.SparkSession
+
+object Accumulator {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName(this.getClass.getName)
+      .master("local")
+      .getOrCreate()
+
+    val longAcc = spark.sparkContext.longAccumulator("SumAccumulator")
+
+    val rdd = spark.sparkContext.parallelize(Array(1, 2, 3))
+
+    rdd.foreach(x => longAcc.add(x))
+
+    println(longAcc.value)
+  }
+}
